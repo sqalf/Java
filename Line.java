cb5566b12ae736ba3342cbcd30a2a76557de8faa
Bruno Azevedo <130415275+sqalf@users.noreply.github.com>
2024-05-21 11:07:08 -0300

    Add files via upload

---
+++ Line.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Line {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        String l1, l2, l3;
+        System.out.println("digite a linha 1: ");
+        l1 = sc.nextLine();
+        System.out.println("digite a linha 2: ");
+        l2 = sc.nextLine();
+        System.out.println("digite a linha 3: ");
+        l3 = sc.nextLine();
+        sc.close();
+
+        System.out.println("digitado: ");
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+
+    }
+}
---
+++ SqrtPowAbs.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+
+public class SqrtPowAbs {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        double x = 3.0;
+        double y = 4.0;
+        double z = -5.0;
+        double a, b, c;
+
+        a = Math.sqrt(x);
+        b = Math.sqrt(y);
+        c = Math.sqrt(25.0);
+        System.out.printf("Raiz de %.2f = %.2f\n", x, a);
+        System.out.println("Raiz de " + y + " = " + b);
+        System.out.println("Raiz de 25.0 = " + c);
+        System.out.println();
+
+        a = Math.pow(x, y);
+        b = Math.pow(x, 2);
+        c = Math.pow(5, 2);
+        System.out.println(x + " elevado a " + y + " = " + a);
+        System.out.println(x + " elevado a 2 = " + b);
+        System.out.println("5 elevado a 2 = " + c);
+        System.out.println();
+
+        a = Math.abs(y);
+        b = Math.abs(z);
+        System.out.printf("Valor absoluto de %.2f = %.2f\n", y, a);
+        System.out.printf("Valor absoluto de %.2f = %.2f", z, b);
+    }
+}
