f4951db593139be723d7240910de3a16e4eb8741
Bruno Azevedo <130415275+sqalf@users.noreply.github.com>
2024-05-21 11:03:52 -0300

    Add files via upload

---
+++ Retangulo.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Retangulo {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("digite o valor da base do retangulo: ");
+        double base = read.nextDouble();
+        System.out.println("digite o valor da altura do retangulo: ");
+        double altura = read.nextDouble();
+
+        double area = base * altura;
+        double perimetro = 2 * (base + altura);
+        double diagonal = Math.sqrt(base * base + altura * altura);
+
+        System.out.printf("Base: %.1f\n", base);
+        System.out.printf("Altura: %.1f\n", altura);
+        System.out.printf("Área: %.4f\n", area);
+        System.out.printf("Perímetro: %.4f\n", perimetro);
+        System.out.printf("Diagonal: %.4f", diagonal);
+
+    }
+}
